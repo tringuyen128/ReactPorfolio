f24c21854c4bed5472053e57043ca132633798d6
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-09 14:42:32 -0700

    working on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Tri Nguyen</h1>
+        <h3 className="text-light">Fullstack Web Developer</h3>
+        <h5 className="text-light">
+          <em>
+            I build websites with a focus on responsive design and
+            accessibility.
+          </em>
+        </h5>
+        <CTA />
+        <a href="#contact" className="scroll__down"></a>
+      </div>
+    </header>
+  )
 }
 
 export default Header
