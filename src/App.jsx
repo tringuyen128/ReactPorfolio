40eb51c31b4eeac408b50b1cbad49a0e93fa3d25
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-09 11:17:26 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Skill from './components/skill/Skill'
+import Project from './components/project/Project'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Skill />
+      <Project />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/skill/Skill.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './skill.css'
+
+const Skill = () => {
+  return <div>Skill</div>
+}
+
+export default Skill
