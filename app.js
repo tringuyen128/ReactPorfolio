dfc65db06a35219effb29d4510af7daf5824737d
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-22 06:47:32 -0700

    update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require('path')
 const port = process.env.PORT || 5000
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static())
+    app.use(express.static('build'))
     app.get('*', (req, res) => {
         req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
     })
