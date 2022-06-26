8aa00b2dd893e3e6a88c6d898727282af82aeaf9
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-26 08:50:22 -0700

    update project

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -14,6 +14,8 @@ const data = [
     title: 'Create Your Thoughts',
     github: 'https://github.com/tringuyen128/social-app-thoughts',
     demo: 'https://social-app-your-thoughts.herokuapp.com/',
+    description:
+      'A complete full-stack MERN app using GraphQL and Apollo Server for interaction with database. React was used for the front end, MongoDB, Mongoose, Express, and Nodejs on the backend.',
   },
   {
     id: 2,
@@ -21,6 +23,8 @@ const data = [
     title: 'Airbnb Clone',
     github: 'https://github.com/tringuyen128/Airbnb-clone-react',
     demo: 'https://airbnb-clone-react-22.herokuapp.com/',
+    description:
+      'The app used props and states to make it dynamic changes. React-date-range was used to build a beautiful custom calendar. Some packages were used to be compatable with the new version of React 18 such as Router, routes, Link, and useNavigate.',
   },
   {
     id: 3,
@@ -28,6 +32,8 @@ const data = [
     title: 'Just Tech News',
     github: 'https://github.com/tringuyen128/just-tech-news',
     demo: 'https://tech-blognews.herokuapp.com/',
+    description:
+      'This a blog website where developers can publish their blog posts and comment on other developers’ posts as well. This application follows the MVC paradigm and it is using Handlebars.js as the templating language, Sequelize as the ORM, and the express-session npm package for authentication.',
   },
   {
     id: 4,
@@ -35,6 +41,8 @@ const data = [
     title: 'Book Search Engine',
     github: 'https://github.com/tringuyen128/Book-Search-Engine',
     demo: 'https://book-search-challenge-21-mern.herokuapp.com/',
+    description:
+      'The Book search engine is an application is leveraging the entire MERN stack and with a React front end, MongoDB database, and Node.js/Express.js server and API. It allows users to save book searches to the back end. The application is built using Google Books API search engine build with GraphQL API and Apollo Server.',
   },
   {
     id: 5,
@@ -42,6 +50,7 @@ const data = [
     title: '',
     github: 'https://github.com',
     demo: 'https://social-app-your-thoughts.herokuapp.com/',
+    description: 'coming soon',
   },
   {
     id: 6,
@@ -49,6 +58,8 @@ const data = [
     title: 'Weather App',
     github: 'https://github.com/tringuyen128/Weather-Dashboard',
     demo: 'https://tringuyen128.github.io/Weather-Dashboard/',
+    description:
+      'Weather dashboard is an application that will run in the browser and feature dynamically updated HTML and CSS. It provides the city weather- description according to the city provided by USER.With this Application User will be able to plan any trip according to the weather of the city . The Application provides 05 days forecast for more activity prevision.',
   },
 ]
 
@@ -59,7 +70,7 @@ const Project = () => {
       <h2> Projects </h2>
 
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, github, demo, description }) => {
           return (
             <article key={id} className="portfolio__item">
               <div className="portfolio__item-image">
@@ -74,6 +85,7 @@ const Project = () => {
                   Live Demo
                 </a>
               </div>
+              <p className="project__description">{description}</p>
             </article>
           )
         })}
