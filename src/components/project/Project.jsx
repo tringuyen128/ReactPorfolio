895917d6ad381c0b13dd83eddb356be60568ee42
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-06-30 14:14:12 -0700

    update projects

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -4,7 +4,7 @@ import IMG1 from '../../assets/project1.jpg'
 import IMG2 from '../../assets/project2.jpg'
 import IMG3 from '../../assets/project3.jpg'
 import IMG4 from '../../assets/project4.jpg'
-import IMG5 from '../../assets/project1.jpg'
+import IMG5 from '../../assets/project5.jpg'
 import IMG6 from '../../assets/project6.jpg'
 
 const data = [
