44e206c56dfc24f49bafc5c68a2e729dbc99e24a
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-07-15 14:14:06 -0700

    update about page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,19 +8,16 @@ import { BsFolderSymlink } from 'react-icons/bs'
 const About = () => {
   return (
     <section id="about">
-      
-
       <div className="container about__container">
         <div className="about__me">
           {/* <div className="about__me-image">
             <img src={ME} alt="image" />
           </div> */}
-          
         </div>
 
         <div className="about__content">
-        <h5>Get To Know</h5>
-      <h2>About Me</h2>
+          <h5>Get To Know</h5>
+          <h2>About Me</h2>
           <div className="about__cards">
             <article className="about__card">
               <FaAward className="about__icon" />
@@ -30,20 +27,27 @@ const About = () => {
             <article className="about__card">
               <RiTeamLine className="about__icon" />
               <h5>Leadership</h5>
-              <small>12+ Years as a Team Leader</small>
+              <small>10+ Years as a Team Leader</small>
             </article>
             <article className="about__card">
               <BsFolderSymlink className="about__icon" />
               <h5>Projects</h5>
-              <small>50+ Projects</small>
+              <small>6+ Projects</small>
             </article>
           </div>
 
           <p>
-            A full-stack web developer located in Silicon Valley California, I enjoy creating things that live on the internet with a full passion for UI effects, animations, and dynamic user experiences. 
-            Complete a full-stack course from UC Berkeley Extension that covers everything to building a complete web app using popular and modern technologies like React, GraphQL, Redux, etc. 
+            A full-stack web developer located in Silicon Valley California, I
+            enjoy creating things that live on the internet with a full passion
+            for UI effects, animations, and dynamic user experiences. Complete a
+            full-stack course from UC Berkeley Extension that covers everything
+            to building a complete web app using popular and modern technologies
+            like React, GraphQL, Redux, etc.
             <br></br>
-            As a well-organized person, a great problem solver, and an experienced team leader with high attention to detail, I successfully worked with teams proven by ambitious projects and inclusive products.  
+            As a well-organized person, a great problem solver, and an
+            experienced team leader with high attention to detail, I
+            successfully worked with teams proven by ambitious projects and
+            inclusive products.
           </p>
           <a href="#contact" className="btn btn-primary">
             Let's Talk
